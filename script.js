65d0e1998df32c830ffc2020c042f09dec91d717
moneykingkor <money.king.kor@gmail.com>
2025-02-25 13:30:07 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const button = document.getElementById("testButton");
+    button.addEventListener("click", function() {
+        alert("Button clicked! Web hosting test successful.");
+    });
+});
